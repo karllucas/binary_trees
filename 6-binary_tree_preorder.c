92f91e3e690e6e75d5e19ce06932cf2b647003c4
Karl Kamakia <karlkamakia@gmail.com>
2022-08-24 21:00:40 +0300

    Update 6-binary_tree_preorder.c

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -1,30 +1,20 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_is_root - function that goes through a binary tree using
- *	pre-order traversal
+ * binary_tree_preorder - goes through a binary tree using
+ * pre-order traversal
  *
- * @tree: pointer to the root node of the tree to traverse
- *
- * Return: 1 if node is a root, otherwise 0
+ * @tree: tree root
+ * @func: pointer to a function to call for each node
+ * Return: no return
  */
-
-/*
- * Pseudocode
- * 1. Print current node if not NULL
- * 2. Print left node by passing it to step 1 recursively
- * 3. Print right node by passing it to step 1 recursively
- */
-void binary_tree_preorder(
-	__attribute__((unused)) const binary_tree_t *tree,
-	__attribute__((unused)) void (*func)(int))
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
-	if (
-		tree != NULL && /* Check: tree is not null, prevent segmentation fault */
-		func != NULL) /* Check: func is not null, prevent segmentation fault*/
-	{
-		func(tree->n);
-		binary_tree_preorder(tree->left, func);	/* Show Left node */
-		binary_tree_preorder(tree->right, func); /* Show Right node */
-	}
+	if (tree == NULL || func == NULL)
+		return;
+
+	func(tree->n);
+
+	binary_tree_preorder(tree->left, func);
+	binary_tree_preorder(tree->right, func);
 }
