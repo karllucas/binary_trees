891a7d9e869f600c79d7f106160e8f07da104ed9
MosesSoftEng <moses.soft.eng@gmail.com>
2022-08-26 12:01:10 +0300

    Task 9 attempt 2

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -7,16 +7,7 @@
  *
  * Return: height root, otherwise 0
  */
-
-/*
- * Pseudocode
- * 1. Get left node height recursively
- * 2. Get right node height recursively
- * 3. Return (the max of step 1 and 2) + 1
- */
-size_t binary_tree_height(
-	__attribute__((unused)) const binary_tree_t *tree
-)
+size_t binary_tree_height(const binary_tree_t *tree)
 {
 	if (tree != NULL)
 	{
@@ -24,16 +15,13 @@ size_t binary_tree_height(
 		int rightHeight = 0;
 
 		if (tree->left)
-			/* Get height left node*/
 			leftHeight = binary_tree_height(tree->left) + 1;
 
 		if (tree->right)
-			/* Get height right node*/
 			rightHeight = binary_tree_height(tree->right) + 1;
 
 		int max = leftHeight;
 
-		/* Get max height between leftHeight and rightHeight */
 		if (rightHeight > max)
 			max = rightHeight;
 
